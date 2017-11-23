11f26e0f7fb80095a33cffa6a2b294bec5dae419
Илья Уткин <ilyautkin@mail.ru>
2017-11-23 16:40:35 +0500

    Prevent multiple file processing

--- core/components/smushit/elements/snippets/snippet.smushit.php
+++ core/components/smushit/elements/snippets/snippet.smushit.php
@@ -2,10 +2,10 @@
 /**
  * smushit
  *
- * This snippet tries to optimise and compress PNG and JPEG images for better
- * performance using the resmush.it optimisation API. This will help massively
- * with Google Page Speed and can reduce image sizes by up to 70%. This will
- * overwrite any existing images so is intended to be used as an output filter
+ * This snippet tries to optimise and compress PNG and JPEG images for better 
+ * performance using the resmush.it optimisation API. This will help massively 
+ * with Google Page Speed and can reduce image sizes by up to 70%. This will 
+ * overwrite any existing images so is intended to be used as an output filter 
  * after pthumb or similar.
  *
  * This uses resmush.it API: http://resmush.it/
@@ -32,34 +32,43 @@ if (!function_exists('smushitFormatBytes')) {
     }
 }
 
+$input = ltrim($input, '/');
+
+// Form name for optimized file
+$dirs = explode('/', $input);
+$file = array_pop($dirs);
+$output = implode('/', $dirs) . '/op-' . $file;
+
 // If image file is not blank and the image exists
 if (!empty($input) && file_exists(MODX_BASE_PATH . $input)) {
-
-    // Variables
-    $site_url = $modx->getOption('site_url');
-    $image = json_decode(file_get_contents('http://api.resmush.it/ws.php?img=' . $site_url . $input));
-    $original = filesize(MODX_BASE_PATH . $input);
-
-    // If there is an error, report it
-    if (isset($image->error)){
-        $modx->log(modX::LOG_LEVEL_ERROR, '[smushit] Could not optimise image: ' . $site_url . $input);
-        return $input;
+    // If optimazed file not exists
+    if (!file_exists(MODX_BASE_PATH . $output)) {
+        // Variables
+        $site_url = rtrim($modx->getOption('site_url'), '/');
+        $image = json_decode(file_get_contents('http://api.resmush.it/ws.php?img=' . $site_url . '/' . $input));
+        $original = filesize(MODX_BASE_PATH . $input);
+        
+        // If there is an error, report it
+        if (isset($image->error)){
+            $modx->log(modX::LOG_LEVEL_ERROR, '[smushit] Could not optimise image: ' . $site_url . '/'  . $input);
+            return $input;
+        }
+    
+        // Save the remote image overwriting the original
+        copy($image->dest, MODX_BASE_PATH . $output) or die("Could not save remote image.");
+        
+        // Get optimised image filesize
+        $optimised = filesize(MODX_BASE_PATH . $output);
+            
+        // Log the savings
+        $modx->log(modX::LOG_LEVEL_DEBUG, "[smushit] $input > Original " . smushitFormatBytes($original) . " vs. optimised " . smushitFormatBytes($optimised) . " " . number_format(100 - (($optimised/$original)*100), 2)  . "% saving.");
     }
-
-    // Save the remote image overwriting the original
-    copy($image->dest, MODX_BASE_PATH . $input) or die("Could not save remote image.");
-
-    // Get optimised image filesize
-    $optimised = filesize(MODX_BASE_PATH . $input);
-
-    // Log the savings
-    $modx->log(modX::LOG_LEVEL_DEBUG, "[smushit] $input > Original " . smushitFormatBytes($original) . " vs. optimised " . smushitFormatBytes($optimised) . " " . number_format(100 - (($optimised/$original)*100), 2)  . "% saving.");
-    return $input;
-
+    return $output;
+    
 } else {
-
+    
     $modx->log(modX::LOG_LEVEL_ERROR, '[smushit] Something is wrong with the input image: ' . MODX_BASE_PATH . $input);
     // Return whatever was provided
     return $input;
-
+    
 }
